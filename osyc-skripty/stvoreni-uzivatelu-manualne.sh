bee496d2f7f16fadacc62e73ae6281380465bf2a
Vít Černý <120108993+vit-cerny@users.noreply.github.com>
2024-06-13 08:25:17 +0200

    Add files via upload

---
+++ osyc-skripty/stvoreni-uzivatelu-manualne.sh
@@ -0,0 +1,14 @@
+sudo useradd -c "Pan Dulezity" pulezity
+sudo passwd Uvodni-Heslo_1
+
+sudo useradd -c "Jakub Hrabal" jhrabal
+sudo passwd Uvodni-Heslo_1
+
+sudo useradd -c "Tereza Simakova" tsimakova
+sudo passwd Uvodni-Heslo_1
+
+sudo useradd -c "Matin Mrazek" mmrazek
+sudo passwd Uvodni-Heslo_1
+
+sudo useradd -c "Jitka Kuliskova" jkuliskova
+sudo passwd Uvodni-Heslo_1
