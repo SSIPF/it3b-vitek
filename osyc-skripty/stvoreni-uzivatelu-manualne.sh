8dfe4f640fbfd0b9cbd13bfb088d8dd7849a816b
Vít Černý <120108993+vit-cerny@users.noreply.github.com>
2024-06-13 08:27:58 +0200

    Update stvoreni-uzivatelu-manualne.sh

--- osyc-skripty/stvoreni-uzivatelu-manualne.sh
+++ osyc-skripty/stvoreni-uzivatelu-manualne.sh
@@ -1,14 +1,23 @@
+#!/bin/bash
+
+sed -i 's/\r$//' stvoreni-uzivatelu-manualne.sh
+chmod +x stvoreni-uzivatelu-manualne.sh
+
+
+# Create users with full names
 sudo useradd -c "Pan Dulezity" pulezity
-sudo passwd Uvodni-Heslo_1
+sudo passwd pulezity
 
 sudo useradd -c "Jakub Hrabal" jhrabal
-sudo passwd Uvodni-Heslo_1
+sudo passwd jhrabal
 
 sudo useradd -c "Tereza Simakova" tsimakova
-sudo passwd Uvodni-Heslo_1
+sudo passwd tsimakova
 
 sudo useradd -c "Matin Mrazek" mmrazek
-sudo passwd Uvodni-Heslo_1
+sudo passwd mmrazek
 
 sudo useradd -c "Jitka Kuliskova" jkuliskova
-sudo passwd Uvodni-Heslo_1
+sudo passwd jkuliskova
+
+echo "Users created successfully."
